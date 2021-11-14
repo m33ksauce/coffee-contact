9f69ea6e18532cc395770299b63754fbccc323d0
Chris Meek <m33ksauce@gmail.com>
2021-11-14 10:54:45 -0600

    Add stylecop rules to Data project

--- ContactCoffee.Data/Messages/SurveyResponseCreated.cs
+++ ContactCoffee.Data/Messages/SurveyResponseCreated.cs
@@ -1,21 +1,24 @@
-using System;
-using System.Collections.Generic;
-
 namespace ContactCoffee.Data.Messages
 {
+    using System;
+    using System.Collections.Generic;
+
     public class SurveyResponseCreated
     {
         public string SurveyID { get; set; }
-        public Dictionary<string, string> Answers { get; set; } = 
+
+        public Dictionary<string, string> Answers { get; set; } =
             new Dictionary<string, string>();
 
-        public override string ToString() {
-            var ret = "";
+        public override string ToString()
+        {
+            var ret = string.Empty;
             ret += $"{this.SurveyID}:";
             foreach (KeyValuePair<string, string> ans in this.Answers)
             {
                 ret += $"{ans.Key}={ans.Value};";
             }
+
             return ret;
         }
     }
